073c83c660d394b035c5f95b640237dac55976b4
Frank Rose <franklin.rose@gmail.com>
2019-05-09 17:09:16 -0400

    Appointment DELETE route

--- api/appointments.js
+++ api/appointments.js
@@ -8,9 +8,13 @@ router.get('/', (req, res, next) =>
 router.post('/', (req, res, next) => {
     const { startTime: time, location, attendeeId, userId } = req.body
     Appointment.create({ time, location, attendeeId, userId }).then(appt => {
-        console.log(appt);
-        res.send('Success')
+        res.json('Successfully created.')
     })
+})
+
+router.delete('/', (req, res, next) => {
+    const { appointmentId: id } = req.body
+    Appointment.destroy({ where: { id } }).then(appt => res.send('Successfully deleted.'))
 })
 
 module.exports = router
