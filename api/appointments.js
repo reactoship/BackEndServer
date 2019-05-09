6ff7471a4d240f5e69a7e388c14a919faaed2ed7
Frank Rose <franklin.rose@gmail.com>
2019-05-09 16:25:28 -0400

    Appointment POST route

--- api/appointments.js
+++ api/appointments.js
@@ -5,4 +5,12 @@ router.get('/', (req, res, next) =>
     Appointment.findAll().then(appts => res.json(appts)).catch(next)
 )
 
+router.post('/', (req, res, next) => {
+    const { startTime: time, location, attendeeId, userId } = req.body
+    Appointment.create({ time, location, attendeeId, userId }).then(appt => {
+        console.log(appt);
+        res.send('Success')
+    })
+})
+
 module.exports = router
