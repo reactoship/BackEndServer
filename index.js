9a6e05b3b0b252db52811e86d49d1b7e1d8dcc0a
Frank Rose <franklin.rose@gmail.com>
2019-05-14 10:41:18 -0400

    Added CORS

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const db = require('./db')
 const sessionStore = new SequelizeStore({ db })
 const PORT = process.env.PORT || 8080
 const app = express()
+const cors = require('cors')
 const socketio = require('socket.io')
 module.exports = app
 
@@ -33,6 +34,9 @@ const createApp = () => {
 
   // compression middleware
   app.use(compression())
+
+  //  CORS middleware
+  app.use(cors())
 
   // session middleware with passport
   app.use(
